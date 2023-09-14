95fb2c2d30f128d68bd9338ef5348534cccdb24a
kevlog <contact.kvnkap@gmail.com>
2023-09-14 14:02:48 +0700

    Update SyncDatabase

--- initializers/syncDatabase.go
+++ initializers/syncDatabase.go
@@ -3,12 +3,12 @@ package initializers
 import (
 	"university-api/course"
 	"university-api/student"
+	"university-api/user"
 
 	"gorm.io/gorm"
 )
 
 func SyncDatabase(db *gorm.DB) error {
-	// err := db.AutoMigrate(user.User{}, student.Student{}, course.Course{})
-	err := db.AutoMigrate( student.Student{}, course.Course{})
+	err := db.AutoMigrate(user.User{}, student.Student{}, course.Course{})
 	return err
 }
